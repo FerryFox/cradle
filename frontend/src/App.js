e433d584dfa3e2af7e30aa31b86caa99c5745662
archfox <dustin.rasch@outlook.de>
2023-08-24 07:12:32 +0200

    some testing in the App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/about" element={<AboutPage />} />
+                    </Routes>
+                </header>
+            </div>
+        </Router>
+    );
+}
+
+function HomePage() {
+    return (
+        <div>
+            <p>Welcome to the Home Page</p>
+        </div>
+    );
+}
+
+function AboutPage() {
+    return (
+        <div>
+            <p>Welcome to the About Page</p>
+        </div>
+    );
 }
 
 export default App;

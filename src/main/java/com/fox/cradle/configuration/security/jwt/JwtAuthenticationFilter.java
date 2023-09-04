3c197441bc17745d18259ea1c6524a9ea9f92c19
archfox <dustin.rasch@outlook.de>
2023-09-04 08:38:43 +0200

    some informal annotations

--- src/main/java/com/fox/cradle/configuration/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/fox/cradle/configuration/security/jwt/JwtAuthenticationFilter.java
@@ -21,32 +21,48 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
+    /*
+    The given code is a portion of a custom filter implementation, specifically for a method
+    doFilterInternal that is often seen in the context of Spring Framework's OncePerRequestFilter.
+    This filter ensures that a specific piece of code is executed once and only once for each request.
+     */
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        if (request.getServletPath().contains("/api/v1/auth")) {
+        //changes from /api/v1/auth to /api/auth
+        if (request.getServletPath().contains("/api/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+
+        //stops searching for the userDetails if the token is null or doesn't start with "Bearer "
+        if (authHeader == null ||!authHeader.startsWith("Bearer "))
+        {
             filterChain.doFilter(request, response);
             return;
         }
+
+        //gets the user's email from the token and sets the userDetails to the user's email
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null)
+        {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
-            if (jwtService.isTokenValid(jwt, userDetails) ) {
+            //checks if the token is valid and sets the authToken to the user's details
+            //also sets the SecurityContext, UsernamePasswordAuthenticationToken, and WebAuthenticationDetailsSource
+            if (jwtService.isTokenValid(jwt, userDetails))
+            {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
+                        //gets the user's authorities which are an empty list atm.
                         userDetails.getAuthorities()
                 );
                 authToken.setDetails(
@@ -54,7 +70,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 );
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
+
         }
+
         filterChain.doFilter(request, response);
     }
 }

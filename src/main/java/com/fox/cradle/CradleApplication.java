23bc7a9a88206919a8b55acd53f72d28dccc63e0
archfox <dustin.rasch@outlook.de>
2023-09-05 10:04:08 +0200

    updating branch

--- src/main/java/com/fox/cradle/CradleApplication.java
+++ src/main/java/com/fox/cradle/CradleApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CradleApplication {
@@ -11,6 +14,7 @@ public class CradleApplication {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(CradleApplication.class, args);
+
 	}
 
 }

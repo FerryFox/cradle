f7ccf26c8344ca2cf0f56906ac818e24f2b6771d
archfox <dustin.rasch@outlook.de>
2023-09-13 23:06:43 +0200

    Added event, creates a new AppUser everytime a successful register is done

---
+++ src/main/java/com/fox/cradle/SecurityHook.java
@@ -0,0 +1,29 @@
+package com.fox.cradle;
+
+import com.fox.cradle.configuration.security.SecuritySender;
+import com.fox.cradle.configuration.security.user.User;
+import com.fox.cradle.features.appuser.model.AppUser;
+import com.fox.cradle.features.appuser.service.AppUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityHook
+{
+    private final AppUserService appUserService;
+
+    @EventListener
+    public void handleSecuritySender(SecuritySender event)
+    {
+        User user = event.getUser();
+        AppUser appUser = new AppUser();
+        //may be handle user id
+        appUser.setAppUserName(user.getFirstname());
+        appUser.setAppUserEmail(user.getEmail());
+        appUser.setReceiveNews(user.isReceiveNews());
+
+        appUserService.save(appUser);
+    }
+}

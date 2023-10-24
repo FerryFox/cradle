2ac0b0bcea55cb431bd04f6dbe8a853731c1d9a1
archfox <dustin.rasch@outlook.de>
2023-10-24 10:30:07 +0200

    change base directory for frontend sonarcloud_frontend.yml

--- src/test/java/com/fox/cradle/features/appuser/service/AppUserServiceTest.java
+++ src/test/java/com/fox/cradle/features/appuser/service/AppUserServiceTest.java
@@ -1,8 +1,5 @@
 package com.fox.cradle.features.appuser.service;
 
-import com.fox.cradle.features.appuser.model.AppUser;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;

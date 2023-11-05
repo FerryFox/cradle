74705cb48b6da250c1cf2a3cea13f78cd76f9518
Dustin Rasch <dustin.rasch@outlook.de>
2023-11-05 17:20:09 +0100

    BlogMapping unit tests

---
+++ src/test/java/com/fox/cradle/features/blog/controller/BlogControllerIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.fox.cradle.features.blog.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlogControllerIntegrationTest {
+
+    @Test
+    void createBlogEntry() {
+    }
+
+    @Test
+    void getLatestBlogEntries() {
+    }
+}
---
+++ src/test/java/com/fox/cradle/features/blog/service/BlogServiceIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.fox.cradle.features.blog.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlogServiceIntegrationTest {
+
+}
